f46380853d3a96f58ad9e46c28e4228c0aacbad9
Sandra <sandrabasquero@gmail.com>
2016-10-12 13:26:17 +0200

    Controls and mute tests

--- miniVideoApp/miniVideoApp/ViewController.m
+++ miniVideoApp/miniVideoApp/ViewController.m
@@ -35,9 +35,10 @@
         self.playerVideo = [[AVPlayerViewController alloc] init];
         self.playerVideo.player = [AVPlayer playerWithURL:urlVideoFile];
         self.playerVideo.view.frame = [[self.videoThumbsBtns objectAtIndex:0]bounds];
-        self.playerVideo.showsPlaybackControls = YES; //NO
+        self.playerVideo.showsPlaybackControls = NO;
         [self.playerVideo.player play];
-        self.playerVideo.view.userInteractionEnabled = YES; //NO
+        self.playerVideo.player.muted = YES;
+        self.playerVideo.view.userInteractionEnabled = NO;
         [self.videoPlayersArray addObject:self.playerVideo]; //Add to videos array
         [[self.videoThumbsBtns objectAtIndex:i] addSubview:self.playerVideo.view];
     }
@@ -46,19 +47,21 @@
 
 - (IBAction)video1Action:(id)sender {
     NSLog(@"Video 1");
-    /*AVPlayerViewController *video = [self.videoPlayersArray objectAtIndex:0];
+    AVPlayerViewController *video = [self.videoPlayersArray objectAtIndex:0];
     video.showsPlaybackControls = YES;
     video.view.userInteractionEnabled = YES;
-    video.modalPresentationStyle = UIModalPresentationOverFullScreen;
+    self.playerVideo.player.muted = NO;
+    /*video.modalPresentationStyle = UIModalPresentationOverFullScreen;
     [self presentViewController:video animated:YES completion:nil];*/
 }
 
 - (IBAction)video2Action:(id)sender {
     NSLog(@"Video 2");
-    /*AVPlayerViewController *video = [self.videoPlayersArray objectAtIndex:1];
+    AVPlayerViewController *video = [self.videoPlayersArray objectAtIndex:1];
     video.showsPlaybackControls = YES;
     video.view.userInteractionEnabled = YES;
-    video.modalPresentationStyle = UIModalPresentationOverFullScreen;
+    self.playerVideo.player.muted = NO;
+    /*video.modalPresentationStyle = UIModalPresentationOverFullScreen;
     [self presentViewController:video animated:YES completion:nil];*/
 }
 
